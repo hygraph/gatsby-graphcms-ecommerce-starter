77bb0b3199c12c34a8c11f6dcf7e2abc52ec8ce3
Jamie Barton <jamie@notrab.dev>
2019-10-14 09:38:43 +0100

    feat: add new enableCartPage option to demo

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
       options: {
         graphcmsEndpoint: `https://api-euwest.graphcms.com/v1/ck0z7rqgm0dnc01b26mj505py/master`,
         graphcmsToken: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJ0b2tlbklkIjoiYTBkZTllMjEtYjUxMi00ZTZhLWJmY2ItZmNhM2MxMzRlM2VkIn0.0-z1hMJHqYdWPQx3eqWYedh3JtT80DTW85xiQZYfepQ`,
+        enableCartPage: true,
       },
     },
   ],
