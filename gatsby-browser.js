e47233c0159d7ecd8c284a8269aaa1b11c7b0956
Jamie Barton <jamie@notrab.dev>
2019-11-25 11:56:37 +0000

    feat: wrap root with cart context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { CartProvider } from 'react-use-cart';
 
 import Layout from './src/components/Layout';
 
+const randomCartId = () =>
+  Math.random()
+    .toString(36)
+    .substring(7);
+
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <CartProvider id={randomCartId()}>{element}</CartProvider>;
 };
