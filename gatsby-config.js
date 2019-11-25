23d5f01dbeae4f9ce9669f6c81c7b23ef0c99d6a
Jonathan Steele <jonathan@steele.pro>
2019-11-25 10:56:15 +0000

    feat: Add, initialise gatsby-source-printful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ require(`dotenv`).config();
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-source-printful`,
+      options: {
+        apiKey: process.env.PRINTFUL_API_KEY,
+      },
+    },
+    {
       resolve: `gatsby-source-graphql`,
       options: {
         typeName: `GraphCMS`,
