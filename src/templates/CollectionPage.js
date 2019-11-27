ec68bb36e71508382f43abb7d2b7fb767c135253
Jonathan Steele <jonathan@steele.pro>
2019-11-27 13:01:41 +0000

    refactor: Change how we query, build pages for products, categories and collections

--- src/templates/CollectionPage.js
+++ src/templates/CollectionPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import ProductGrid from '../components/ProductGrid';
 
-function CollectionPage({ pageContext: { collection, products = [] } }) {
+function CollectionPage({
+  data: {
+    cms: { collection },
+  },
+}) {
   return (
     <React.Fragment>
       <h1 className="font-bold text-6xl mb-3 text-slategray">
@@ -11,9 +16,40 @@ function CollectionPage({ pageContext: { collection, products = [] } }) {
 
       <hr className="border-b border-gainsboro w-10" />
 
-      <ProductGrid products={products} />
+      <ProductGrid products={collection.products} />
     </React.Fragment>
   );
 }
+
+export const pageQuery = graphql`
+  query CollectionQuery($slug: String!) {
+    cms {
+      collection(where: { slug: $slug }) {
+        title
+        slug
+        products {
+          id
+          description {
+            markdown
+          }
+          name
+          printfulProductId
+          printfulProduct {
+            productImage {
+              childImageSharp {
+                fluid(maxWidth: 560) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            variants {
+              retail_price
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default CollectionPage;
--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-import ReviewsList from '../components/ReviewsList';
-
-function ProductPage({ pageContext: { product, reviews } }) {
+function ProductPage({
+  data: {
+    cms: { product },
+  },
+}) {
   return (
     <React.Fragment>
       <h1 className="font-bold text-5xl mb-3 text-slategray">{product.name}</h1>
-
-      <ReviewsList reviews={reviews} />
     </React.Fragment>
   );
 }
+
+export const pageQuery = graphql`
+  query ProductQuery($id: ID!) {
+    cms {
+      product(where: { id: $id }) {
+        name
+        printfulProductId
+        printfulProduct {
+          productImage {
+            childImageSharp {
+              fluid(maxWidth: 560) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          variants {
+            retail_price
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default ProductPage;
