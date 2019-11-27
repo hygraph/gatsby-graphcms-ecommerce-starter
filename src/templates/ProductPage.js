a8a11ab02431c01ba3f78ea2e489dce9cb0eb427
Jonathan Steele <jonathan@steele.pro>
2019-11-27 15:20:57 +0000

    feat: Initial design of product page
    With variant selection

--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 function ProductPage({
   data: {
     cms: { product },
   },
 }) {
+  const [activeVariantId, setActiveVariantId] = useState(null);
+
+  const activeVariant = product.printfulProduct.variants.find(
+    variant => variant.id === activeVariantId
+  );
+
   return (
     <React.Fragment>
-      <h1 className="font-bold text-5xl mb-3 text-slategray">{product.name}</h1>
+      <div className="mb-6">
+        <h1 className="font-bold text-6xl mb-3 text-slategray">
+          {product.name}
+        </h1>
+        <hr className="border-b border-gainsboro w-10" />
+      </div>
+      <div className="md:flex -mx-4">
+        <div className="mb-8 px-4 md:mb-0 md:w-2/5">
+          <div className="cursor-pointer w-full overflow-hidden relative p-3 bg-gainsboro">
+            <Img
+              fluid={
+                activeVariant
+                  ? activeVariant.variantImage.childImageSharp.fluid
+                  : product.printfulProduct.productImage.childImageSharp.fluid
+              }
+              alt={product.name}
+              title={product.name}
+            />
+          </div>
+        </div>
+        <div className="px-4 md:w-3/5">
+          <p className="font-semibold text-2xl">
+            {activeVariant &&
+              new Intl.NumberFormat('en-GB', {
+                style: 'currency',
+                currency: 'EUR',
+              }).format(activeVariant.retail_price / 100)}
+          </p>
+          {product.description && (
+            <p className="leading-loose text-lightgray text-sm">
+              {product.description.markdown}
+            </p>
+          )}
+          <select onChange={e => setActiveVariantId(e.target.value)}>
+            {product.printfulProduct.variants.map((variant, index) => {
+              const [, splitVariantName] = variant.name.split(' - ');
+
+              return (
+                <option key={index} value={variant.id}>
+                  {splitVariantName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
@@ -17,6 +69,9 @@ export const pageQuery = graphql`
   query ProductQuery($id: ID!) {
     cms {
       product(where: { id: $id }) {
+        description {
+          markdown
+        }
         name
         printfulProductId
         printfulProduct {
@@ -28,7 +83,16 @@ export const pageQuery = graphql`
             }
           }
           variants {
+            id
+            name
             retail_price
+            variantImage {
+              childImageSharp {
+                fluid(maxWidth: 560) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
