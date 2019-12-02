c36cfe83bf0b17da2ebf8053f67cfabde7b78f66
Jonathan Steele <jonathan@steele.pro>
2019-12-02 13:58:54 +0000

    refactor: Use default locale from config

--- src/context/Locale.js
+++ src/context/Locale.js
@@ -1,5 +1,7 @@
 import React, { createContext, useEffect, useReducer } from 'react';
 
+import locales from '../../config/locales';
+
 const LocaleContext = createContext();
 
 function reducer(state, { type, locale }) {
@@ -11,8 +13,10 @@ function reducer(state, { type, locale }) {
   }
 }
 
-function LocaleProvider({ children, locale = 'EN' }) {
-  const [state, dispatch] = useReducer(reducer, { activeLocale: 'EN' });
+const defaultLocale = locales.find(locale => locale.default);
+
+function LocaleProvider({ children, locale = defaultLocale.path }) {
+  const [state, dispatch] = useReducer(reducer, { activeLocale: locale });
 
   useEffect(() => {
     updateLocale(locale);
