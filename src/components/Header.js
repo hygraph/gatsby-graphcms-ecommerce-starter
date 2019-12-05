e5f1443c1ac2c0dc134124dd5afb1ec46e9105d9
Jonathan Steele <jonathan@steele.pro>
2019-12-05 12:56:36 +0000

    fix: Tidy locale select

--- src/components/Header.js
+++ src/components/Header.js
@@ -85,16 +85,29 @@ function Header() {
               </li>
             ))}
           </ul>
-          <select
-            value={activeLocale}
-            onChange={({ target: { value } }) => updateLocale(value)}
-          >
-            {locales.map(({ locale, path }, index) => (
-              <option key={index} value={path}>
-                {locale}
-              </option>
-            ))}
-          </select>
+          <div className="mx-4 relative">
+            <select
+              value={activeLocale}
+              className="block appearance-none bg-white border border-gainsboro px-4 py-3 pr-8 focus:outline-none focus:bg-white text-lightgray focus:text-slategray rounded"
+              onChange={({ target: { value } }) => updateLocale(value)}
+            >
+              {locales.map(({ label, path }, index) => (
+                <option key={index} value={path}>
+                  {label}
+                </option>
+              ))}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-lightgray">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+
           <div>
             <Link to="/cart" className="flex items-center">
               {!isEmpty && (
