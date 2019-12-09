b2725463d301bbc99a60b118b9504b16986ac8ec
Jonathan Steele <jonathan@steele.pro>
2019-12-09 11:52:39 +0000

    feat: Add Printful API datasource to Apollo Server
    With createOrder method to interact with API

---
+++ functions/graphql/datasources/printful.js
@@ -0,0 +1,26 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class PrintfulAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.printful.com';
+  }
+
+  willSendRequest(request) {
+    request.headers.authorization = `Basic ${Buffer.from(
+      process.env.PRINTFUL_API_KEY
+    ).toString('base64')}`;
+  }
+
+  async createOrder(recipient, items) {
+    try {
+      const { result: data } = await this.post(`orders`, { recipient, items });
+
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+module.exports = PrintfulAPI;
