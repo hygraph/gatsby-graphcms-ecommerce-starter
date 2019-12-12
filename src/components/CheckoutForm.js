9f980626f644c7e21a0974b5b1493a027b8eb560
Jamie Barton <jamie@notrab.dev>
2019-12-12 10:09:09 +0000

    simplify checkout form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -16,13 +16,25 @@ const CHECKOUT_MUTATION = `mutation checkout($name: String!, $email: String!, $t
   }
 }`;
 
+const defaultValues = {
+  separateBilling: false,
+};
+
 function CheckoutPage({ stripe }) {
+  const {
+    handleSubmit,
+    register,
+    watch,
+    setValue,
+    setError,
+    errors,
+    formState,
+  } = useForm({ defaultValues });
+  const { isSubmitting } = formState;
   const [checkout] = useMutation(CHECKOUT_MUTATION);
   const { cartTotal, items } = useCart();
-  const { handleSubmit, register, watch, setValue } = useForm();
-  const useSeparateBilling = watch('separateBilling', false);
-  const [checkoutError, setCheckoutError] = useState(null);
-  const [cardElement, setCardElement] = useState(null);
+  const values = watch();
+  const useSeparateBilling = !!values.separateBilling;
 
   useEffect(() => {
     register({ name: 'stripe' });
@@ -57,7 +69,9 @@ function CheckoutPage({ stripe }) {
         },
       });
     } catch (err) {
-      setCheckoutError(
+      setError(
+        'checkout',
+        'unableToProceed',
         err.message || 'Unable to process order. Please try again.'
       );
     }
@@ -67,10 +81,18 @@ function CheckoutPage({ stripe }) {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 pb-0 md:pb-0 my-3 md:my-6">
+      <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
         <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6">
-          Contact information
+          Shipping
         </h3>
+
+        <div className="mb-3 md:mb-6">
+          <Input
+            name="shipping.name"
+            placeholder="Name"
+            register={register({ required: true })}
+          />
+        </div>
 
         <div className="md:flex -mx-3">
           <div className="md:w-1/2 mb-3 md:mb-6 px-3">
@@ -90,20 +112,6 @@ function CheckoutPage({ stripe }) {
               register={register}
             />
           </div>
-        </div>
-      </div>
-
-      <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
-        <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6">
-          Shipping
-        </h3>
-
-        <div className="mb-3 md:mb-6">
-          <Input
-            name="shipping.name"
-            placeholder="Name"
-            register={register({ required: true })}
-          />
         </div>
 
         <div className="mb-3 md:mb-6">
@@ -157,143 +165,87 @@ function CheckoutPage({ stripe }) {
           </div>
         </div>
 
-        <div className="flex items-center justify-between">
+        <div>
           <Checkbox name="separateBilling" register={register}>
             Use different billing address
           </Checkbox>
-
-          <button
-            type="submit"
-            className="bg-primary text-white px-3 py-2 h-10 focus:outline-none font-bold"
-          >
-            Continue to {useSeparateBilling ? 'billing' : 'payment'}
-          </button>
         </div>
       </div>
 
-      {/* <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
-            <div className="flex items-start justify-between">
-              <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6 inline-flex items-center">
-                <span className="text-primary">
-                  <svg
-                    className="fill-current w-8 mr-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                  </svg>
-                </span>
-                Ship to
-              </h3>
-
-              <button className="appearance-none text-primary focus:outline-none">
-                Edit
-              </button>
-            </div>
-
-            <p>Jamie Barton</p>
-            <p>123 Address Street, Line 2, Washington, United States, 12345</p>
-          </div> */}
-
       {useSeparateBilling && (
-        <React.Fragment>
-          <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
-            <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6">
-              Billing
-            </h3>
+        <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
+          <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6">
+            Billing
+          </h3>
 
-            <div className="mb-3 md:mb-6">
+          <div className="mb-3 md:mb-6">
+            <Input
+              name="billing.name"
+              placeholder="Name"
+              register={register({ required: true })}
+            />
+          </div>
+
+          <div className="mb-3 md:mb-6">
+            <Input
+              name="billing.address1"
+              placeholder="Address"
+              register={register({ required: true })}
+            />
+          </div>
+
+          <div className="mb-3 md:mb-6">
+            <Input
+              name="billing.address2"
+              placeholder="Apartment, suite, etc. (optional)"
+              register={register}
+            />
+          </div>
+
+          <div className="md:flex -mx-3">
+            <div className="md:w-1/2 mb-3 md:mb-6 px-3">
               <Input
-                name="billing.name"
-                placeholder="Name"
+                name="billing.city"
+                placeholder="City"
+                register={register({ required: true })}
+              />
+            </div>
+            <div className="md:w-1/2 mb-3 md:mb-6 px-3">
+              <Input
+                name="billing.state"
+                placeholder="State / County"
+                register={register({ required: true })}
+              />
+            </div>
+          </div>
+
+          <div className="md:flex -mx-3">
+            <div className="md:w-1/2 mb-3 md:mb-6 px-3">
+              <Input
+                name="billing.country"
+                placeholder="Country"
                 register={register({ required: true })}
               />
             </div>
 
-            <div className="mb-3 md:mb-6">
+            <div className="md:w-1/2 mb-3 md:mb-6 px-3">
               <Input
-                name="billing.address1"
-                placeholder="Address"
+                name="billing.zip"
+                placeholder="ZIP / Postcode"
                 register={register({ required: true })}
               />
             </div>
-
-            <div className="mb-3 md:mb-6">
-              <Input
-                name="billing.address2"
-                placeholder="Apartment, suite, etc. (optional)"
-                register={register}
-              />
-            </div>
-
-            <div className="md:flex -mx-3">
-              <div className="md:w-1/2 mb-3 md:mb-6 px-3">
-                <Input
-                  name="billing.city"
-                  placeholder="City"
-                  register={register({ required: true })}
-                />
-              </div>
-              <div className="md:w-1/2 mb-3 md:mb-6 px-3">
-                <Input
-                  name="billing.state"
-                  placeholder="State / County"
-                  register={register({ required: true })}
-                />
-              </div>
-            </div>
-
-            <div className="md:flex -mx-3">
-              <div className="md:w-1/2 mb-3 md:mb-6 px-3">
-                <Input
-                  name="billing.country"
-                  placeholder="Country"
-                  register={register({ required: true })}
-                />
-              </div>
-
-              <div className="md:w-1/2 mb-3 md:mb-6 px-3">
-                <Input
-                  name="billing.zip"
-                  placeholder="ZIP / Postcode"
-                  register={register({ required: true })}
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-end">
-              <button
-                type="submit"
-                className="bg-primary text-white px-3 py-2 h-10 focus:outline-none font-bold"
-              >
-                Continue to payment
-              </button>
-            </div>
           </div>
-          <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
-            <div className="flex items-start justify-between">
-              <h3 className="text-slategray text-2xl md:text-4xl font-bold mb-6 inline-flex items-center">
-                <span className="text-primary">
-                  <svg
-                    className="fill-current w-8 mr-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                  </svg>
-                </span>
-                Bill to
-              </h3>
 
-              <button className="appearance-none text-primary focus:outline-none">
-                Edit
-              </button>
-            </div>
-
-            <p>Jamie Barton</p>
-            <p>123 Address Street, Line 2, Washington, United States, 12345</p>
+          <div className="flex items-center justify-end">
+            <button
+              type="submit"
+              className="bg-primary text-white px-3 py-2 h-10 focus:outline-none font-bold"
+            >
+              Continue to payment
+            </button>
           </div>
-        </React.Fragment>
+        </div>
       )}
 
       <div className="rounded bg-white border-2 border-gainsboro p-3 md:p-6 my-3 md:my-6">
@@ -306,14 +258,21 @@ function CheckoutPage({ stripe }) {
             className="appearance-none bg-white border-2 border-slategray px-4 py-3 pr-8 focus:outline-none focus:border-primary focus:bg-white text-slategray focus:outline-none w-full rounded"
             hidePostalCode={true}
             onChange={handleStripeChange}
-            onReady={el => setCardElement(el)}
+            onReady={el => setValue('cardElement', el)}
           />
+
+          {values.stripe && values.stripe.error && (
+            <span className="text-red text-sm pt-3">
+              {values.stripe.error.message}
+            </span>
+          )}
         </div>
 
         <div className="flex items-center justify-end">
           <button
             type="submit"
-            className="bg-primary text-white px-3 py-2 h-10 focus:outline-none font-bold"
+            className="bg-primary rounded text-white px-3 py-2 h-10 focus:outline-none font-bold"
+            disabled={isSubmitting}
           >
             Pay for order
           </button>
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,14 +5,10 @@ import CheckoutForm from '../components/CheckoutForm';
 
 function CheckoutPage() {
   return (
-    <div className="lg:flex -mx-3">
-      <div className="lg:w-2/3 px-3">
-        <Elements>
-          <CheckoutForm />
-        </Elements>
-      </div>
-
-      <div className="lg:w-1/3 px-3">Cart summary</div>
+    <div className="max-w-4xl mx-auto">
+      <Elements>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 }
