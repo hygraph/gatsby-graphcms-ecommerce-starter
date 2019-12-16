8a68aecf9b04a8ed85e3d5e18b10ac704f689dea
Jamie Barton <jamie@notrab.dev>
2019-12-16 10:47:28 +0000

    feat: add notification to cart interactions (#50)
    * feat: add notification to cart interactions
    
    * update padding

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,27 @@
 import 'isomorphic-fetch';
+import 'react-toastify/dist/ReactToastify.css';
 
 import React from 'react';
 import { CartProvider } from 'react-use-cart';
 import { GraphQLClient, ClientContext } from 'graphql-hooks';
+import { ToastContainer } from 'react-toastify';
 
 import StripeProvider from './src/components/StripeProvider';
 import Layout from './src/components/Layout';
+import {
+  handleItemAdded,
+  handleItemUpdated,
+  handleItemRemoved,
+} from './src/utils/cart-helpers';
+
+const toastOptions = {
+  position: 'top-center',
+  draggable: false,
+  toastClassName: 'bg-primary text-white text-center p-2 shadow-none',
+  progressClassName: 'h-0',
+  closeButton: false,
+  autoClose: 2000,
+};
 
 const client = new GraphQLClient({
   url: '/.netlify/functions/graphql',
@@ -24,7 +40,15 @@ export const wrapRootElement = ({ element }) => {
   return (
     <StripeProvider>
       <ClientContext.Provider value={client}>
-        <CartProvider id={randomCartId()}>{element}</CartProvider>
+        <CartProvider
+          id={randomCartId()}
+          onItemAdd={handleItemAdded}
+          onItemUpdate={handleItemUpdated}
+          onItemRemove={handleItemRemoved}
+        >
+          {element}
+        </CartProvider>
+        <ToastContainer {...toastOptions} />
       </ClientContext.Provider>
     </StripeProvider>
   );
