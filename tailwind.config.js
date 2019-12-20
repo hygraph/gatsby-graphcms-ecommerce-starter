1a8400ad790ddc2a4a7b7b774ca0bfcf799a6aa1
Jamie Barton <jamie@notrab.dev>
2019-12-20 13:31:55 +0000

    refactor stars to rating component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       white: '#fff',
       primary: '#5828e8',
       gainsboro: 'rgba(0, 0, 0, 0.1)',
-      lightgray: 'rgba(0, 0, 0, 0.25)',
+      lightgray: 'rgba(0, 0, 0, 0.5)',
       slategray: '#101b42',
       red: '#DC143C',
     },
