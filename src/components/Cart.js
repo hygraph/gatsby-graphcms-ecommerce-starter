5326bbdfdcd85a77f54b3f2b9c84da5611ad70dd
Jamie Barton <jamie@notrab.dev>
2019-12-23 13:15:25 +0000

    tidy up cart item

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -18,13 +18,14 @@ function Cart() {
       <CartItemList />
 
       <div className="mt-3 md:mt-6 py-3 md:py-6 border-t-2 border-gainsboro">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col">
+        <div className="flex flex-col items-end">
+          <div className="flex flex-col items-end mb-3">
             <span className="text-slategray">Sub total</span>
             <span className="text-xl font-bold text-primary">
               {formattedSubTotal}
             </span>
           </div>
+
           <Link
             to="/checkout"
             className="bg-primary hover:bg-slategray px-4 py-3 rounded text-white text-sm font-bold tracking-widest uppercase focus:outline-none"
