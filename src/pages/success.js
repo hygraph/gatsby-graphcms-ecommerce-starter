5dbd79aff009783046ec68f86752505aa99a08f9
Jonathan Steele <jonathan@steele.pro>
2020-01-10 09:52:53 +0000

    fix: Prevent access to /success if not transitioned from checkout

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
 import SEO from '../components/SEO';
 
-function SuccessPage({
-  location: {
-    state: { orderId },
-  },
-}) {
+function SuccessPage({ location }) {
+  const showSuccessScreen = location.state && location.state.orderId;
+
+  useEffect(() => {
+    if (!showSuccessScreen) {
+      const navigateTimer = setTimeout(() => {
+        navigate(`/`);
+      }, 3000);
+
+      return () => clearTimeout(navigateTimer);
+    }
+  }, [showSuccessScreen]);
+
+  if (!showSuccessScreen)
+    return (
+      <React.Fragment>
+        <div className="mb-6">
+          <h1 className="font-bold text-6xl mb-3 text-primary">Oops!</h1>
+
+          <hr className="border-b border-gainsboro w-10" />
+        </div>
+        <p>You've no business being on this page!</p>
+      </React.Fragment>
+    );
+
   return (
     <React.Fragment>
       <SEO pageTitle="Thank you for your order" />
@@ -18,7 +39,9 @@ function SuccessPage({
 
       <p>
         Please take note of your order reference for your records:{' '}
-        <span className="font-semibold text-lightgray">{orderId}</span>
+        <span className="font-semibold text-lightgray">
+          {location.state.orderId}
+        </span>
       </p>
     </React.Fragment>
   );
