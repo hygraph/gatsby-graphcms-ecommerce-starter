d1796c23cba8833e1a3c2d96d29e8f6b3254835a
Jonathan Steele <jonathan@steele.pro>
2020-04-23 15:12:22 +0100

    refactor: Update order-created function to reflect new webhooks payload

--- functions/deploy-site.js
+++ functions/deploy-site.js
@@ -4,52 +4,18 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 const BUILD_HOOK = process.env.BUILD_HOOK;
-const BUILD_STAGE_TRIGGER = process.env.STAGE_TRIGGER;
 
-const BUILD_TRIGGERS = [
-  'createProduct',
-  'updateProduct',
-  'deleteProduct',
-  'createCategory',
-  'updateCategory',
-  'deleteCategory',
-  'createCollection',
-  'updateCollection',
-  'deleteCollection',
-];
-
-exports.handler = async event => {
-  if (!event.body)
-    return {
-      statusCode: 405,
-    };
-
-  const {
-    stageName,
-    info: { fieldName },
-  } = JSON.parse(event.body);
-
+exports.handler = async () => {
+  console.log(process.env.BUILD_HOOK);
   try {
-    if (
-      stageName === BUILD_STAGE_TRIGGER &&
-      BUILD_TRIGGERS.includes(fieldName)
-    ) {
-      await fetch(BUILD_HOOK, {
-        method: 'post',
-      });
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Build successfully triggered.',
-        }),
-      };
-    }
+    await fetch(BUILD_HOOK, {
+      method: 'post',
+    });
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'No build required.',
+        message: 'Build successfully triggered.',
       }),
     };
   } catch (err) {
